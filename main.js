154e2ac5d7c4d5f5418034bcf9a2d0c5a163cf79
ammr64 <ammr64@outlook.com>
2020-12-07 19:05:03 +0200

    edit the text field

--- main.js
+++ main.js
@@ -27,6 +27,22 @@ function salonsignupfunc(){
     document.getElementById("login-content").style.display="none";
     document.getElementById("login-content1").style.display="flex";
 }
+
+(function(){
+
+    document.getElementById("passDiv").classList.remove('focus');
+    document.getElementById("emailDiv").classList.remove('focus');
+
+    document.getElementById("fname").classList.remove('focus');
+    document.getElementById("lname").classList.remove('focus');
+    document.getElementById("date").classList.remove('focus');
+    //document.getElementById("email").classList.remove('focus');
+    document.getElementById("phoneNum").classList.remove('focus');
+    document.getElementById("rpassDiv").classList.remove('focus');
+
+
+})()
+
 // function myFunction() {
 //     let parent = this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 //     if(this.value == ""){
@@ -35,10 +51,15 @@ function salonsignupfunc(){
 // }
 
 // function focusfunc(){
+//     // document.getElementById('pass')
+//
 //     document.querySelector(".input-div.focus:before").style.width="50%";
 //     document.querySelector(".input-div.focus:after").style.width="50%";
 //     document.querySelector(".input-div.focus > div > h5").style.top="5px";
 //     document.querySelector(".input-div.focus > div > h5").style.fontsize="5px";
 //     document.querySelector(".input-div.focus > .i > i").style.color="#bc8500";
-//
 // }
+
+function test(){
+    alert('say hello!');
+}
