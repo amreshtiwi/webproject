40f9db76a5bbcbf2fd5c6d0c4e2516d299b5853b
ammr64 <ammr64@outlook.com>
2020-12-10 23:10:03 +0200

    some editing in customer page

--- main.js
+++ main.js
@@ -36,30 +36,230 @@ function salonsignupfunc(){
     document.getElementById("fname").classList.remove('focus');
     document.getElementById("lname").classList.remove('focus');
     document.getElementById("date").classList.remove('focus');
+
+
     //document.getElementById("email").classList.remove('focus');
     document.getElementById("phoneNum").classList.remove('focus');
     document.getElementById("rpassDiv").classList.remove('focus');
+    document.getElementById("license_salon").classList.remove('focus');
+    document.getElementById("city").classList.remove('focus');
+    document.getElementById("street").classList.remove('focus');
+    document.getElementById("website").classList.remove('focus');
+    document.getElementById("days").classList.remove('focus');
+    document.getElementById("from_time").classList.remove('focus');
+    document.getElementById("to_time").classList.remove('focus');
+    document.getElementById("rrpassDiv").classList.remove('focus');
 
 
 })()
-
-// function myFunction() {
-//     let parent = this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-//     if(this.value == ""){
-//         parent.classList.remove("focus");
-//     }
-// }
-
-// function focusfunc(){
-//     // document.getElementById('pass')
-//
-//     document.querySelector(".input-div.focus:before").style.width="50%";
-//     document.querySelector(".input-div.focus:after").style.width="50%";
-//     document.querySelector(".input-div.focus > div > h5").style.top="5px";
-//     document.querySelector(".input-div.focus > div > h5").style.fontsize="5px";
-//     document.querySelector(".input-div.focus > .i > i").style.color="#bc8500";
-// }
-
-function test(){
-    alert('say hello!');
+//for the time button in the customer page
+function dlgCancel(){
+    dlgHide();
 }
+
+function dlgOK(){
+    dlgHide();
+    //implement code to save data
+}
+
+function dlgHide(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+
+function showDialog(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+}
+
+// for the log out button in customer page
+function dlgCancel1(){
+    dlgHide1();
+}
+
+function dlgOK1(){
+    dlgHide1();
+    window.location.replace("logIn.html");
+}
+
+function dlgHide1(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox1");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+
+function showDialog1(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox1");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+
+}
+//for the setting button in customer page
+
+function dlgCancel2(){
+    dlgHide2();
+}
+
+function dlgOK2(){
+    dlgHide2();
+    window.location.replace("logIn.html");
+}
+
+function dlgHide2(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox2");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+
+function showDialog2(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox2");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+
+}
+//show password changer dialog
+
+function dlgCancel3(){
+    dlgHide3();
+}
+
+function dlgOK3(){
+    dlgHide3();
+    //implement code to save data
+}
+
+function dlgHide3(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox3");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+function showDialog3(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox3");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+
+}
+
+function changePassDialog(){
+    dlgHide2();
+    showDialog3();
+}
+//end show password changer dialog
+
+//show information data dialog
+
+function infoDialog(){
+    dlgHide2();
+    showDialog4();
+}
+
+function showDialog4(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox4");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+
+}
+
+function dlgCancel4(){
+    dlgHide4();
+}
+
+function dlgOK4(){
+    dlgHide4();
+    //implement code to save data
+}
+
+function dlgHide4(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox4");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+
+//end show information data dialog
+
+//show delete dialoge
+function deleteDialog(){
+    dlgHide2();
+    showDialog5();
+}
+
+function showDialog5(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox5");
+    whitebg.style.display = "block";
+    dlg.style.display = "block";
+
+    var winWidth = window.innerWidth;
+
+    dlg.style.left = (winWidth/2) - 480/2 + "px";
+    dlg.style.top = "150px";
+
+}
+
+
+function dlgCancel5(){
+    dlgHide5();
+}
+
+function dlgOK5(){
+    dlgHide5();
+    //implement code to save data
+}
+
+function dlgHide5(){
+    var whitebg = document.getElementById("white-background");
+    var dlg = document.getElementById("dlgbox5");
+    whitebg.style.display = "none";
+    dlg.style.display = "none";
+}
+//end show delete dialoge
+
+// (document).ready(function(){
+//     (".default_option").click(function(){
+//         (".dropdown ul").addClass("active");
+//     });
+//
+//     (".dropdown ul li").click(function(){
+//         var text = (this).text();
+//         (".default_option").text(text);
+//         (".dropdown ul").removeClass("active");
+//     });
+// });
+//
---
+++ main2.js
@@ -0,0 +1,40 @@
+(function(){
+
+    document.getElementById("phoneNum").classList.remove('focus');
+
+    document.getElementById("rrpassDiv").classList.remove('focus');
+    document.getElementById("license_salon").classList.remove('focus');
+    document.getElementById("city").classList.remove('focus');
+    document.getElementById("street").classList.remove('focus');
+    document.getElementById("website").classList.remove('focus');
+    document.getElementById("days").classList.remove('focus');
+    document.getElementById("from_time").classList.remove('focus');
+    document.getElementById("to_time").classList.remove('focus');
+
+
+
+})()
+
+
+// (function(){
+//
+//     document.getElementById("passDiv").classList.remove('focus');
+//     document.getElementById("emailDiv").classList.remove('focus');
+//
+//     document.getElementById("fname").classList.remove('focus');
+//     document.getElementById("lname").classList.remove('focus');
+//     document.getElementById("date").classList.remove('focus');
+//     //document.getElementById("email").classList.remove('focus');
+//     document.getElementById("phoneNum").classList.remove('focus');
+//     document.getElementById("rpassDiv").classList.remove('focus');
+//     document.getElementById("license_salon").classList.remove('focus');
+//     document.getElementById("city").classList.remove('focus');
+//     document.getElementById("street").classList.remove('focus');
+//     document.getElementById("website").classList.remove('focus');
+//     document.getElementById("days").classList.remove('focus');
+//     document.getElementById("from_time").classList.remove('focus');
+//     document.getElementById("to_time").classList.remove('focus');
+//     document.getElementById("rrpassDiv").classList.remove('focus');
+//
+//
+// })()
