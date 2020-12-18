95c807cc6206d65252b6ae96e1a1304fe5732801
ramisir3 <ramisir3@gmail.com>
2020-12-18 19:42:57 +0200

    edit info customer

---
+++ editInfoCustomer.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if (isset($_POST['email'], $_POST['phone'])) {
+    if ($_POST['email'] == "" || $_POST['phone'] == "") {
+        echo '<span style="color: darkred">الرجاء ادخال كلا الحقلين</span>';
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo '<span style="color: darkred">الرجاء ادخال بريد الكتروني صحيح</span>';
+        } else {
+            $phone = test_input($_POST['phone']);
+            if (!is_numeric($phone)) {
+                echo '<span style="color: darkred">الرجاء ادخال رقم صحيح</span>';
+            } else {
+                $user = 'root';
+                $pass = '';
+                $db = new PDO('mysql:host=localhost;dbname=halaqi', $user, $pass);
+                $db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $CID = $_SESSION['UserID'];
+                $sql = 'update customer set Email=:email, PhoneNum=:num where CID=:CID';
+                $serviceResult = $db->prepare($sql);
+                $serviceResult->execute(array(
+                    ':CID' => $CID,
+                    ':email'=> $email,
+                    ':num'=>$phone
+                ));
+                echo '<span style="color: darkred">تم تحديث البيانات بنجاح</span>';
+            }
+        }
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = htmlentities($data);
+
+    return $data;
+}
---
+++ getInfoCustomer.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$user = 'root';
+$pass = '';
+$db = new PDO('mysql:host=localhost;dbname=halaqi', $user, $pass);
+$db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$CID=$_SESSION['UserID'];
+$sql = 'select Email,PhoneNum from customer where CID=:CID';
+$serviceResult = $db->prepare($sql);
+$serviceResult->execute(array(
+    ':CID' => $CID,
+));
+$row = $serviceResult->fetch(PDO::FETCH_ASSOC);
+$email = $row['Email'];
+$phone = $row['PhoneNum'];
+?>
+
+<div class="login-content">
+
+    <div id="emailDiv" class="input-div two focus">
+        <div class="i">
+            <i class="fas fa-lock"></i>
+        </div>
+        <div>
+            <h5 class="mar">البريد الإلكتروني</h5>
+            <input id="emalDiv" class="input1" type="email" name="emailDiv" value="<?php echo $email ?>"
+                    style="background: none; outline: none; border: none; position: relative;top: -5px;">
+        </div>
+    </div>
+
+    <div id="PhoneDiv" class="input-div two focus">
+        <div class="i">
+            <i class="fas fa-lock"></i>
+        </div>
+        <div>
+            <h5 class="mar">رقم الهاتف</h5>
+            <input id="phonediv" class="input1" type="text" name="phoneDiv" pattern="\d{10}" maxlength="10"
+                   title="Only number of 10 digits" value="<?php echo $phone ?>"  style="background: none; outline: none; border: none; position: relative;top: -5px;">
+        </div>
+    </div>
+</div>
